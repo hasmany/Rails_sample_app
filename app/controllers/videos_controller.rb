7186afa73380ee95a9b04fb4df4d41b5210a0f25
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-04-28 10:47:00 -0500

    working on update action

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -18,10 +18,26 @@ class VideosController < ApplicationController
 
   end
 
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    # @video.attributes(video_params)
+    @video = Vidoe.update(video_params)
+    redirect_to root_path
+    # Notice, our show and edit actions are exaclty the same!
+    # in both cases we grabbign an existing instance of Video
+    # to do something
+  end
+
   private
 
   def video_params
     params.require(:video).permit(:title, :description, :youtube_id)
+  end
+
   end
 
 end
